d486f66804a6b66eced0f16381df87acfed997f4
TimoKaufmann <kaufmann.timo.95@gmail.com>
2025-08-25 13:03:13 +0200

    feat(UI-104): ResourceCard Komponente entwickelt

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import './App.css'
+import ResourceCard from './components/ResourceCard.jsx'
 
 function App() {
+
+  const dummyResource = {
+    id: 1,
+    title: 'Einführung in React Hooks',
+    description: 'Ein umfassender Leitfaden zu useState, useEffect und useContext.'
+  }
 
   return (
       <div className="min-h-screen bg-gray-100 font-sans antialiased">
         <header className="bg-gradient-to-r from-blue-600 to-indigo-700 p-4 shadow-lg">
           <div className="container mx-auto flex justify-between items-center">
-            <h1 className="text-3xl font-extrabold text-white">Ressourcen-Katalog</h1>
+            <h1 className="text-3xl font-extrabold text-white">
+              Ressourcen-Katalog
+            </h1>
             <nav>
-              {/*Hier kommt später das Menü. */}
+              {/* Hier kommt spaeter das Menu. */}
             </nav>
           </div>
         </header>
 
         <main className="container mx-auto p-4 mt-4">
-          <p className="text-gray-700 text-lg"> Willkommen im Ressourcen-Katalog!</p>
+          <div className="max-w-md">
+            <ResourceCard resource={dummyResource} />
+          </div>
         </main>
       </div>
   )
